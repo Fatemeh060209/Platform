1ff2ae2f6381235ba1f33a697f8275c47d838b87
Osama-Almostafa <61210365+Osama-Almostafa@users.noreply.github.com>
2020-06-16 17:39:20 +0200

    Commmit 4

--- src/main/java/javaFx/AppGUIController.java
+++ src/main/java/javaFx/AppGUIController.java
@@ -26,18 +26,24 @@ import java.util.LinkedList;
 public class AppGUIController implements PulsListener, EkgListener {
 
     public Button logIn; // referenser til controllers fra FXML-filen
-    public Button load;
-    public Label tempLabel;
-    public TextField id;
-    public TextArea TempDataOutput;
+    public Button loadPuls;
+    public Label pulsLabel;
+    public TextField idPuls;
+    public TextArea pulsDataOutput;
 
     public EkgDAO ekgDAO = new EkgDAOImplement(); // opretter en objekt af klaseem TempDAO
     public PulsDAO pulsDAO = new PulsDAOImplement();
     public TextField EKGID;
     public Button EkgStart;
-    public Button Back;
-    public Button EKGLog;
+    public Button BackEKG;
+    public Button loadEkg;
     public Polyline EKGGraf;
+    public Button LogIn;
+    public Button EKGScene;
+    public Button PulsScene;
+    public Button pulsStart;
+    public TextArea ekgDataOutput;
+    public Button BackPuls;
 
     public void ekgSampler(ActionEvent actionEvent) { // Event Driving Programming
         ThreadPC threadPC = new ThreadPC(this);
@@ -51,7 +57,7 @@ public class AppGUIController implements PulsListener, EkgListener {
         pulsGenerator.registerObserver(this);
     }
 
-    public void back(ActionEvent actionEvent) throws IOException { // Event Driving Programming som skifter mellem scene
+    public void LogIn(ActionEvent actionEvent) throws IOException { // Event Driving Programming som skifter mellem scene
         Parent firstPaneLoader = FXMLLoader.load(getClass().getResource("/LogIn.fxml"));
         Scene firstScene = new Scene(firstPaneLoader);
         Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
@@ -62,20 +68,46 @@ public class AppGUIController implements PulsListener, EkgListener {
 
     public void patientData(ActionEvent actionEvent) throws IOException { //  Event Driving Programming,
         // som skifter mellem scene
-        Parent secondPaneLoader = FXMLLoader.load(getClass().getResource("/patientFolder.fxml"));
+        Parent secondPaneLoader = FXMLLoader.load(getClass().getResource("/PatientData.fxml"));
         Scene secondScene = new Scene(secondPaneLoader);
         Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         primaryStage.setScene(secondScene);
-        primaryStage.setTitle("Patient's folder");
+        primaryStage.setTitle("Patient's Data");
         primaryStage.show();
     }
 
-    public void load(ActionEvent actionEvent) throws IOException {
-        Parent thirdPaneLoader = FXMLLoader.load(getClass().getResource("/EKGLoad.fxml"));
+    public void ekgScene(ActionEvent actionEvent) throws IOException {
+        Parent thirdPaneLoader = FXMLLoader.load(getClass().getResource("/EKG.fxml"));
         Scene thirdScene = new Scene(thirdPaneLoader);
         Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         primaryStage.setScene(thirdScene);
-        primaryStage.setTitle("Data Base");
+        primaryStage.setTitle("EKG");
+        primaryStage.show();
+    }
+
+    public void pulsScene(ActionEvent actionEvent) throws IOException {
+        Parent fourthPaneLoader = FXMLLoader.load(getClass().getResource("/Puls.fxml"));
+        Scene fourthScene = new Scene(fourthPaneLoader);
+        Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        primaryStage.setScene(fourthScene);
+        primaryStage.setTitle("Puls");
+        primaryStage.show();
+    }
+
+    public void loadEkg(ActionEvent actionEvent) throws IOException {
+        Parent fifthPaneLoader = FXMLLoader.load(getClass().getResource("/EKGLoad.fxml"));
+        Scene fifthScene = new Scene(fifthPaneLoader);
+        Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        primaryStage.setScene(fifthScene);
+        primaryStage.setTitle("EKG Data Base");
+        primaryStage.show();
+    }
+    public void loadPuls(ActionEvent actionEvent) throws IOException {
+        Parent sixthPaneLoader = FXMLLoader.load(getClass().getResource("/PulsLoad.fxml"));
+        Scene sixthScene = new Scene(sixthPaneLoader);
+        Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        primaryStage.setScene(sixthScene);
+        primaryStage.setTitle("Puls Data Base");
         primaryStage.show();
     }
 
@@ -86,33 +118,30 @@ public class AppGUIController implements PulsListener, EkgListener {
             public void run() {
                 for (int i = 0; i < ekgDTOList.size(); i++) {
                     EkgDTO ekgDTO = ekgDTOList.get(i);
-                    tempLabel.setText(String.valueOf(ekgDTO.getEKG_voltage())); // her bliver værdien angivet som hentes fra DTO
-                    String text = TempDataOutput.getText();
+                    pulsLabel.setText(String.valueOf(ekgDTO.getEKG_voltage())); // her bliver værdien angivet som hentes fra DTO
+                    String text = pulsDataOutput.getText();
                     text += "New Data! Temp:" + ekgDTO.getEKG_voltage() + " °C" + ", TimeStamp: " + ekgDTO.getEKG_time() + "\r\n";
-                    TempDataOutput.setText(text); // her bliver der oprettet en variabel/text, og tildeles hvad der skal stå
+                    pulsDataOutput.setText(text); // her bliver der oprettet en variabel/text, og tildeles hvad der skal stå
                     // i text area
-                    ekgDTO.setPatient_Id(Integer.parseInt(id.getText())); // værdien fra ID/textField sættes ind i setID
+                    ekgDTO.setPatient_Id(Integer.parseInt(idPuls.getText())); // værdien fra ID/textField sættes ind i setID
                 }
                 ekgDAO.savebatch(ekgDTOList); // her gemmes der værdier fra TempDTO
             }
         });
-        // der laves en ny objekt af klassen TempDTO
-
     }
-
 
     public void PulsNotify(PulsDTO pulsDTO) {
         Platform.runLater(new Runnable() { // her bliver der udskrevet løbende temperatur og tid til
             // graiskbrugergrænseflade hvis der er værdier fra sensoren + gemme data i databasen.
             public void run() {
-                tempLabel.setText(String.valueOf(pulsDTO.getPuls_Målinger())); // her bliver værdien angivet som hentes fra DTO
-                String text = TempDataOutput.getText();
+                pulsLabel.setText(String.valueOf(pulsDTO.getPuls_Målinger())); // her bliver værdien angivet som hentes fra DTO
+                String text = pulsDataOutput.getText();
                 text += "New Data! Temp:" + pulsDTO.getPuls_Målinger() + " °C" + ", TimeStamp: " + pulsDTO.getPuls_time() + "\r\n";
-                TempDataOutput.setText(text); // her bliver der oprettet en variabel/text, og tildeles hvad der skal stå
+                pulsDataOutput.setText(text); // her bliver der oprettet en variabel/text, og tildeles hvad der skal stå
                 // i text area
             }
         });
-        pulsDTO.setPatient_ID(Integer.parseInt(id.getText())); // værdien fra ID/textField sættes ind i setID
+        pulsDTO.setPatient_ID(Integer.parseInt(idPuls.getText())); // værdien fra ID/textField sættes ind i setID
         pulsDTO.setPuls_Målinger(pulsDTO.getPuls_Målinger());// Temperatur
         pulsDTO.setPuls_time(pulsDTO.getPuls_time()); // Tid
         pulsDAO.save(pulsDTO); // her gemmes der værdier fra TempDTO
--- src/main/java/javaFx/LoadGUIController.java
+++ src/main/java/javaFx/LoadGUIController.java
@@ -18,31 +18,58 @@ import java.util.List;
 
 public class LoadGUIController {
 
-    public Button søg;  // referenser til controls fra FXML-filen
-    public Button back;
-    public TextField idField;
-    public TextArea TempLoad;
+    public Button PulsSøg;  // referenser til controls fra FXML-filen
+    public Button pulsBack;
+    public Button EKGBack;
+    public Button EKGSøg;
+    public TextField idPulsField;
+    public TextArea plusLoad;
+    public TextField idEKGPuls;
+    public TextArea EKGLoad;
 
-    public void LoadData(ActionEvent actionEvent) { //  Event Driving Programming
+
+    public void LoadEKGData(ActionEvent actionEvent) { //  Event Driving Programming
 
         // oprettes en ny variabel og tildeles værdien fra idField
         EkgDAO ekgDAO = new EkgDAOImplement(); // oprettes en objekt af TempImpl
-        List<EkgDTO> tempData = ekgDAO.load(idField.getText()); // listen som indeholder tempDTO'er udfyldes af resultset der
+        List<EkgDTO> tempData = ekgDAO.load(idPulsField.getText()); // listen som indeholder tempDTO'er udfyldes af resultset der
         // hentes fra laode
         String text = "";
         for (EkgDTO data : tempData) { // for hvert element i listen tempData tilføjes elements data til stringen text
             text += "ID: " + data.getPatient_Id() + ", Time: " + data.getEKG_time() + ", Temperature: " + data.getEKG_voltage() + " °C" + "\r\n";
         }
-        TempLoad.setText(text); // indsætter i text area
+        plusLoad.setText(text); // indsætter i text area
     }
 
-    public void patientFolder(ActionEvent actionEvent) throws IOException { //   Event Driving Programming,
-        // som skifter mellem scene
-        Parent secondPaneLoader = FXMLLoader.load(getClass().getResource("/patientFolder.fxml"));
-        Scene secondScene = new Scene(secondPaneLoader);
+    public void LoadPulsData(ActionEvent actionEvent) { //  Event Driving Programming
+
+        // oprettes en ny variabel og tildeles værdien fra idField
+        EkgDAO ekgDAO = new EkgDAOImplement(); // oprettes en objekt af TempImpl
+        List<EkgDTO> tempData = ekgDAO.load(idPulsField.getText()); // listen som indeholder tempDTO'er udfyldes af resultset der
+        // hentes fra laode
+        String text = "";
+        for (EkgDTO data : tempData) { // for hvert element i listen tempData tilføjes elements data til stringen text
+            text += "ID: " + data.getPatient_Id() + ", Time: " + data.getEKG_time() + ", Temperature: " + data.getEKG_voltage() + " °C" + "\r\n";
+        }
+        plusLoad.setText(text); // indsætter i text area
+    }
+
+    public void ekgScene(ActionEvent actionEvent) throws IOException {
+        Parent thirdPaneLoader = FXMLLoader.load(getClass().getResource("/EKG.fxml"));
+        Scene thirdScene = new Scene(thirdPaneLoader);
         Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        primaryStage.setScene(secondScene);
-        primaryStage.setTitle("Patient's folder");
+        primaryStage.setScene(thirdScene);
+        primaryStage.setTitle("EKG");
         primaryStage.show();
     }
+
+    public void pulsScene(ActionEvent actionEvent) throws IOException {
+        Parent fourthPaneLoader = FXMLLoader.load(getClass().getResource("/Puls.fxml"));
+        Scene fourthScene = new Scene(fourthPaneLoader);
+        Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        primaryStage.setScene(fourthScene);
+        primaryStage.setTitle("Puls");
+        primaryStage.show();
+    }
+
 }
